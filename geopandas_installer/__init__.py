80c19966e18963db615cdd4144b6ac52483e41bf
Mateusz Tarczyński <58234909+MrMT-95@users.noreply.github.com>
2022-07-05 11:00:37 +0200

    Update __init__.py
    download folder changed to archived

--- geopandas_installer/__init__.py
+++ geopandas_installer/__init__.py
@@ -42,7 +42,7 @@ def get_dependencies_links():
 
 
 def download_geopandas_dependencies(python_version, windows_bit_version, temp_dir):
-    website_url = 'https://download.lfd.uci.edu/pythonlibs/a4hvik9m/'
+    website_url = 'https://download.lfd.uci.edu/pythonlibs/archived/'
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0'}
 
     whl_names = get_dependencies_links()
